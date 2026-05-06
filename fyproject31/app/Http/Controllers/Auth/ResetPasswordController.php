ff3e46c65e21b5c24a3475cec27fcc12f74d6689
DHGamingxd <danialhakim256@gmail.com>
2026-05-06 22:55:00 +0800

    database shared

--- fyproject31/app/Http/Controllers/Auth/ResetPasswordController.php
+++ fyproject31/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -33,7 +33,7 @@ class ResetPasswordController extends Controller
             ],
             function ($user, $password) {
                 $user->forceFill([
-                    'password' => Hash::make($password),
+                    'password' => $password,
                 ])->setRememberToken(Str::random(60));
 
                 $user->save();
