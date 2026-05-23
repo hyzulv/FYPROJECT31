07f19394e7e68f936ef1513684921e4449d8092d
DHGamingxd <danialhakim256@gmail.com>
2026-05-23 12:45:10 +0800

    Test connection github/hostinger

--- fyproject31/resources/views/homepage.blade.php
+++ fyproject31/resources/views/homepage.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
-    # Bootstrap CSS and Google Fonts AAAAA
+    # Bootstrap CSS and Google Fonts BBBBB
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;800;900&display=swap" rel="stylesheet">
     <style>
