b541f152c1c5e1ccb269689b9d4f4b13a64c3a6c
DHGamingxd <danialhakim256@gmail.com>
2026-06-14 22:10:32 +0800

    pop up session expired

--- fyproject31/bootstrap/app.php
+++ fyproject31/bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
             '/payment/callback',
+            '/api/session/heartbeat',
         ]);
         $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR |
             Request::HEADER_X_FORWARDED_HOST |
