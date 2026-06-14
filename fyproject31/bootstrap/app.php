967f3f7899db0920ca1c9224cb56d96c1097cc3d
DHGamingxd <danialhakim256@gmail.com>
2026-06-14 21:50:56 +0800

    Revert "page expired log out fix"
    This reverts commit 661ad459d2735ba049eff09be0a67b5e76aa8963.

--- fyproject31/bootstrap/app.php
+++ fyproject31/bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -25,9 +24,5 @@ return Application::configure(basePath: dirname(__DIR__))
         );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->render(function (HttpException $e, Request $request) {
-            if ($e->getStatusCode() === 419) {
-                return redirect()->route('login')->with('error', 'Your session has expired. Please log in again.');
-            }
-        });
+        //
     })->create();
